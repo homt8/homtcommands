afad88984b95ebb7cde936f2e2e6a73acbcd75f2
homthack <arturovega.888@gmail.com>
2018-04-17 15:44:25 -0400

    Add files via upload

---
+++ homtexternal_cmd/init.lua
@@ -0,0 +1,33 @@
+-- External Command (external_cmd) mod by homthack
+-- Allows server commands / chat from outside minetest
+-- License: LGPL
+
+local admin = minetest.setting_get("name")
+
+if admin == nil then
+	admin = "SERVER"
+end
+
+minetest.register_globalstep(
+	function(dtime)
+		f = (io.open(minetest.get_worldpath("external_cmd").."/message", "r"))
+		if f ~= nil then
+			local message = f:read("*line")
+			f:close()
+			os.remove(minetest.get_worldpath("external_cmd").."/message")
+
+			if message ~= nil then
+				local cmd, param = string.match(message, "^/([^ ]+) *(.*)")
+				if not param then
+					param = ""
+				end
+				local cmd_def = minetest.chatcommands[cmd]
+				if cmd_def then
+					cmd_def.func(admin, param)
+				else
+					minetest.chat_send_all(admin..": "..message)
+				end
+			end
+		end
+	end
+)
